d4688e987535f96c3647da4b711bffc58776b3ac
SergeiMelman <s.melman@metocean.co.nz>
2021-10-29 15:09:31 +1300

    wxtiles v2.0.7, add: legend,

--- src/dev-deckgl-index.ts
+++ src/dev-deckgl-index.ts
@@ -11,6 +11,8 @@ import {
 	LibSetupObject,
 	WxTilesLayerManager,
 	WxTilesLayer,
+	createLegend,
+	WxGetColorStyles,
 } from '@metoceanapi/wxtiles-deckgl';
 import '@metoceanapi/wxtiles-deckgl/dist/es/wxtilesdeckgl.css';
 
@@ -29,9 +31,9 @@ async function start() {
 	setWxTilesLogging(true); // logging on
 
 	const params: WxServerVarsStyleType =
-		['obs-radar.rain.nzl.national', 'reflectivity', 'rain.EWIS'];
+		// ['obs-radar.rain.nzl.national', 'reflectivity', 'rain.EWIS'];
 		// ['ecwmf.global', ['wind.speed.eastward.at-10m', 'wind.speed.northward.at-10m'], 'Wind Speed2'];
-		// ['ecwmf.global', 'air.temperature.at-2m', 'Sea Surface Temperature'];
+		['ecwmf.global', 'air.temperature.at-2m', 'Sea Surface Temperature'];
 	const extraParams = {
 		// DeckGl layer's common parameters
 		opacity: 0.5,
@@ -56,12 +58,21 @@ async function start() {
 	// const layerManager = new WxTilesLayerManager({ deckgl, props: wxProps });
 
 	await layerManager.renderCurrentTimestep();
-	
+
 	UIhooks(layerManager);
 	debugLayers(deckgl, wxProps.maxZoom);
+
+	const legendCanvasEl = document.getElementById('legend') as HTMLCanvasElement;
+	if (!legendCanvasEl) return;
+
+	const style = WxGetColorStyles()[params[2]];
+	const legend = createLegend(legendCanvasEl.width - 50, style);
+
+	const txt = params[2] + ' (' + legend.units + ')';
+	drawLegend({ legend, txt, canvas: legendCanvasEl });
 }
 
-function debugLayers(deckgl: Deck, maxZoom?: number) {
+function debugLayers(deckgl: Deck, maxZoom?: number | null) {
 	const debugLayerRed = new DebugTilesLayer({
 		id: 'debugtilesR',
 		data: { color: [255, 0, 0, 120] },
@@ -96,6 +107,76 @@ function UIhooks(layerManager: WxTilesLayerManager) {
 		isPlaying && play();
 		playButton.innerHTML = isPlaying ? 'Stop' : 'Play';
 	});
+}
+
+function drawLegend({ legend, canvas, txt }) {
+	if (!canvas || !legend) return;
+
+	const { width, height } = canvas;
+	const halfHeight = (16 + height) >> 2;
+
+	// draw legend
+	const ctx = canvas.getContext('2d');
+	const imData = ctx.createImageData(width, height);
+	const im = new Uint32Array(imData.data.buffer);
+	im.fill(-1);
+
+	const startX = 2;
+	const startY = 2;
+	const startXY = startX + width * startY;
+
+	const trSize = halfHeight >> 1;
+	// left triangle
+	if (legend.showBelowMin) {
+		const c = legend.colors[0];
+		if (c) {
+			for (let x = 0; x < trSize; ++x) {
+				for (let y = trSize; y < trSize + x; ++y) {
+					im[startXY + x + y * width] = c;
+					im[startXY + x + (trSize * 2 - y) * width] = c;
+				}
+			}
+		}
+	}
+
+	for (let x = 0; x < legend.size; ++x) {
+		for (let y = 0; y < halfHeight; ++y) {
+			if (legend.colors[0]) {
+				im[startX + x + trSize + (y + startY + 1) * width] = legend.colors[x];
+			}
+		}
+	}
+
+	// right triangle
+	if (legend.showAboveMax) {
+		const c = legend.colors[legend.colors.length - 1];
+		if (c) {
+			for (let x = 0; x <= trSize; ++x) {
+				for (let y = trSize; y < trSize + x; ++y) {
+					im[startXY + trSize * 2 + legend.size - x + y * width] = c;
+					im[startXY + trSize * 2 + legend.size - x + (trSize * 2 - y) * width] = c;
+				}
+			}
+		}
+	}
+
+	ctx.putImageData(imData, 0, 0);
+
+	// draw ticks
+	ctx.font = '8px sans-serif';
+	ctx.beginPath();
+	for (const tick of legend.ticks) {
+		ctx.strokeStyle = '#000';
+		ctx.moveTo(tick.pos + trSize + startX + 1, startY + 3);
+		ctx.lineTo(tick.pos + trSize + startX + 1, halfHeight);
+		ctx.fillText(tick.dataString, tick.pos + trSize + startX + 1, halfHeight + 11);
+	}
+	ctx.font = '12px sans-serif';
+	ctx.fillText(txt, 13, height - 5);
+	ctx.stroke();
+
+	ctx.strokeStyle = '#888';
+	ctx.strokeRect(1, 1, width - 3, height - 2); //for white background
 }
 
 start();
